c8904af3d150d7eb223d3f4e2e31f199577a2a9d
praveen <praveenhiremath923@gmail.com>
2017-04-16 19:35:04 +0530

    added google map.

--- Let'sConnect/Let'sConnect/Let'sConnect/ViewControllers/GooglePlace/LCGooglePlaceViewController.m
+++ Let'sConnect/Let'sConnect/Let'sConnect/ViewControllers/GooglePlace/LCGooglePlaceViewController.m
@@ -9,7 +9,7 @@
 #import "LCGooglePlaceViewController.h"
 @import GoogleMaps;
 
-@interface LCGooglePlaceViewController ()
+@interface LCGooglePlaceViewController ()<GMSMapViewDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *addressLabel;
 @end
@@ -17,6 +17,22 @@
 @implementation LCGooglePlaceViewController
 - (void)viewDidLoad {
     [super viewDidLoad];
+        GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:1.285
+                                                                longitude:103.848
+                                                                     zoom:12];
+    
+        GMSMapView *mapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+        mapView.delegate = self;
+        CLLocationCoordinate2D position = CLLocationCoordinate2DMake(51.5, -0.127);
+        self.view = mapView;
+        GMSMarker *london = [GMSMarker markerWithPosition:position];
+        london.title = @"London";
+        london.icon = [UIImage imageNamed:@"poker_3.jpg"];
+        london.map = mapView;
+}
+
+- (void)mapView:(GMSMapView *)mapView didTapInfoWindowOfMarker:(GMSMarker *)marker {
+    // your code
 }
 
 - (void)didReceiveMemoryWarning {
